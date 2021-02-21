796bd69a11fad64f04adcbf34a3be560f1e20e41
ganeshnaidu <ganeshnaidu@users.noreply.github.com>
2021-02-21 20:57:08 +0530

    refactor

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 'use strict'
 
 module.exports = function (arr) {
-    if (!Array.isArray(arr)) {
+    if (!Array.isArray(arr))
         return Promise.reject(TypeError('invalid arguments'));
-    }
 
-    if (!arr.length) {
+    if (!arr.length)
         return Promise.resolve([]);
-    }
 
     return new Promise((resolve, reject) => {
         (function shuffle(arr, length) {
+            if (length === 0) return resolve(arr);
+
             length--;
-            if (length > 0) {
-                const rand = Math.floor(Math.random() * length);
+            const rand = Math.floor(Math.random() * length);
 
-                if (rand != length) {
-                    const tmp = arr[length];
-                    arr[length] = arr[rand];
-                    arr[rand] = tmp;
-                }
-
-                setImmediate(shuffle, arr, length);
-            } else {
-                return resolve(arr);
+            if (rand != length) {
+                const tmp = arr[length];
+                arr[length] = arr[rand];
+                arr[rand] = tmp;
             }
+
+            setImmediate(shuffle, arr, length);
         })(Array.from(arr), arr.length);
     });
 };
